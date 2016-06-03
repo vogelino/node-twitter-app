97987cacdb6a8a147d8ea1b0e7d2a0f216d3a3ba
vogelino <contact@vogelino.com>
2016-06-03 18:12:45 +0200

    style(logger): Wrote reduced code size in logger

--- src/logger.js
+++ src/logger.js
@@ -6,12 +6,10 @@ const my = {};
 my.log = (title, messages, response, color, icon = '') => {
 	let tColor = chalk.inverse[color].bold;
 	let mColor = chalk[color];
-	console.log(icon ? tColor.white(` ${icon} `) : '', tColor(` ${title} `));
+	const iconIfThere = icon ? tColor.white(` ${icon} `) : '';
+	console.log(iconIfThere, tColor(` ${title} `));
 	messages.forEach((msg) => console.log(mColor(`- ${msg}`)));
-	if (response) {
-		console.log('\n', mColor(response));
-	}
-	console.log('\n');
+	console.log(response ? `\n${mColor(response)}\n` : '\n');
 };
 
 that.logError = (title, messages, error) => {
